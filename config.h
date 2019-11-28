c0d9670843e4f433d85bfb9924714ccd51cca005
Thomaz Andrade <thomaz.andradee@gmail.com>
2019-11-28 12:18:09 -0400

    Apply centered master tiles patch

--- config.h
+++ config.h
@@ -42,13 +42,15 @@ static const Rule rules[] = {
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-static const int gappx = 5;
+static const int gappx = 12;
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+  { "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
